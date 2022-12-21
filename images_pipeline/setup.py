618980932e397e7f03259bacb976f906e6444701
Jonathan <joni7840850@gmail.com>
2022-12-21 13:43:28 -0600

    Add basic setup file

---
+++ images_pipeline/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+root_package = "images_pipeline"
+root_package_dir = "src/"
+setup(
+    name="images_pipeline",
+    version="0.0.1",
+    description="A package to process images",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/JonathanHidalgoN",
+    author="Jonathan Hidalgo",
+    author_email="ja.hidalgonunez@ugto.mx",
+    license="MIT",
+    classifiers=["Programming Language :: Python :: 3"],
+    packages=find_packages(where= root_package_dir ),
+    python_requires=">=3.6",
+    package_dir={"": root_package_dir}
+)
