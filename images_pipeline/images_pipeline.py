1b2b891f19684513c419224a4b943ae2ee21da16
Jonathan <joni7840850@gmail.com>
2022-12-20 20:15:01 -0600

    Implement a method to add species

--- images_pipeline/images_pipeline.py
+++ images_pipeline/images_pipeline.py
@@ -13,20 +13,38 @@ class DogsDataset:
     def __init__(self, path : str) -> None:
 
         self.path = path
+        self._species = self.get_species
 
     @property
-    def get_species(self) ->list:
-            """
-            Returns a list of classes in the dataset.
-            """
-            species = os.listdir(self.path)
-            return [specie.split('-')[-1] for specie in species]
+    def species(self) ->list:
+        """
+        Returns a list of classes in the dataset.
+        Returns:
+        A list of strings representing the classes in the dataset.
+        """
+        species = os.listdir(self.path)
+        return [specie.split('-')[-1] for specie in species]
 
-    
-    
+    @species.setter
+    def add_species(self, new_species : list) -> None:
+        """
+        Adds new classes to the dataset.
+        Args:
+            new_species: A list of strings representing the new classes to add.
+        """
+        self._species.extend(new_species)
+        #Now make new folders for the new species
+        #TO DO: check duplicates
+        for new_specie in new_species:
+            os.mkdir(os.path.join(self.path, new_specie))
+                
+
 
 
 if __name__ == "__main__":
     images_path = "images/Images"
     dataset = DogsDataset(path = images_path)
     print(dataset.get_species)
+    new_species = ["new_specie_1", "new_specie_2"]
+    dataset.add_species = new_species
+    print(dataset.get_species[-2:])
