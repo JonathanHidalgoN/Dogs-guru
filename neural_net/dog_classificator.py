b9fc4c8d55943c7508c1c32a1346d50b7deae657
Jonathan <joni7840850@gmail.com>
2022-12-23 12:26:59 -0600

    Add dataloader

--- neural_net/dog_classificator.py
+++ neural_net/dog_classificator.py
@@ -18,3 +18,18 @@ def shuffle_data(total_images : int, proportion : float = .8) -> list:
     shuffle(random_list)
     return random_list[:to_train], random_list[to_train:]
 
+train_parameters = {"resize" : (256,56),
+                   "crop" : 224,
+                   "path" : "/kaggle/input/stanford-dogs-dataset/images/Images",
+                   "batch_size" : 128,
+                   "shuffle" : True,
+                   "proportion" : .8}
+
+if __name__ == "__main__":
+    composed = transforms.Compose([
+        Rescale(train_parameters["resize"]),RandomCrop(train_parameters["crop"]),ToTensor()])
+    transformed_dataset = DogsDataSet(path = train_parameters["path"], transform = composed)
+    total_images = len(transformed_dataset)
+    train_index , test_index = shuffle_data(total_images, train_parameters["proportion"])
+    dataloader = DataLoader(transformed_dataset, batch_size=train_parameters["batch_size"], shuffle=train_parameters["shuffle"], num_workers=4)
+    
