98e3303be2ca9efcc933851b3cd55d5ae2ab71ae
Jonathan <joni7840850@gmail.com>
2022-12-27 10:39:05 -0600

    Template of how this should look, need to fix errors

--- neural_net/dog_classificator.py
+++ neural_net/dog_classificator.py
@@ -1,24 +1,49 @@
-import torchvision
-import torch
+from TrainDogsNet import TrainDogsNet
 from images_pipeline.DogsDataSet import DogsDataSet
 from images_pipeline.Transformations import Rescale, RandomCrop, ToTensor
-from images_pipeline.utils import generate_indexes, count_total_images
-from torchvision import transforms
+from images_pipeline.utils import (
+    generate_indexes,
+    count_total_images,
+    count_total_classes,
+)
 from torch.utils.data import DataLoader
-from time import time as time_time
-from typing import List
+from torch.nn import CrossEntropyLoss as nn_CrossEntropyLoss
+from torch.nn import Linear as nn_Linear
+from torch import optim as torch_optim
+from torch.optim import lr_scheduler as torch_lr_scheduler
+from torch import device as torch_device
+from torch.cuda import is_available as torch_cuda_is_available
+from torchvision import transforms
+from torchvision.models import resnet18 as models_resnet18
 
-total_images = count_total_images("/kaggle/input/stanford-dogs-dataset/images/Images")
+total_images = count_total_images("images/Images")
+total_classes = count_total_classes("images/Images")
 train_parameters = {
     "resize": (256, 256),
     "crop": 224,
-    "path": "/kaggle/input/stanford-dogs-dataset/images/Images",
+    "path": "images/Images",
     "batch_size": 128,
     "shuffle": False,
-    "proportion": 0.8,
     "index_generator": generate_indexes(total_images, [0.8, 0.1, 0.1]),
 }
 
+# Myabe parameters should be in a separate file, or in a function that returns a dictionary.
+model = models_resnet18(pretrained=True)
+num_ftrs = model.fc.in_features
+model.fc = nn_Linear(num_ftrs, total_classes)
+device = torch_device("cuda:0" if torch_cuda_is_available() else "cpu")
+model.to(device)
+optimizer = torch_optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+scheduler = torch_lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+criterion = nn_CrossEntropyLoss()
+
+neural_net_parameters = {
+    "model": model,
+    "optimizer": optimizer,
+    "scheduler": scheduler,
+    "criterion": criterion,
+}
+# ------------------------------------
 if __name__ == "__main__":
     composed = transforms.Compose(
         [
@@ -55,122 +80,9 @@ if __name__ == "__main__":
         num_workers=4,
     )
 
-
-class TrainDogsNet:
-
-    """
-    This class is used to train a neural network on the DogsDataSet.
-    Attributes:
-        model: A torchvision.models object representing the neural network to train.
-        criterion: A torch.nn.modules.loss object representing the loss function to use.
-        optimizer: A torch.optim.sgd.SGD object representing the optimizer to use.
-        scheduler: A torch.optim.lr_scheduler object representing the scheduler to use.
-    """
-
-    def __init__(
-        self,
-        model: torchvision.models,
-        criterion: torch.nn.modules.loss,
-        optimizer: torch.optim.sgd.SGD,
-        scheduler: torch.optim.lr_scheduler,
-    ):
-        self._model = model
-        self._criterion = criterion
-        self._optimizer = optimizer
-        self._scheduler = scheduler
-
-    # Define the properties of the class and their setters, maybe not necessary but won't hurt.
-    # ------------------------------------
-    @property
-    def model(self) -> torchvision.models:
-        return self._model
-
-    @model.setter
-    def model(self, model: torchvision.models) -> None:
-        self._model = model
-
-    @property
-    def criterion(self) -> torch.nn.modules.loss:
-        return self._criterion
-
-    @criterion.setter
-    def criterion(self, criterion: torch.nn.modules.loss) -> None:
-        self._criterion = criterion
-
-    @property
-    def optimizer(self) -> torch.optim.sgd.SGD:
-        return self._optimizer
-
-    @optimizer.setter
-    def optimizer(self, optimizer: torch.optim.sgd.SGD) -> None:
-        self._optimizer = optimizer
-
-    @property
-    def scheduler(self) -> torch.optim.lr_scheduler:
-        return self._scheduler
-
-    @scheduler.setter
-    def scheduler(self, scheduler: torch.optim.lr_scheduler) -> None:
-        self._scheduler = scheduler
-
-    # ------------------------------------
-
-    def _enconde_labels(self, labels: List[str], number_of_classes : int) -> torch.Tensor:
-        """
-        This method is used to encode the labels of the dataset.
-        Args:
-            labels: A list of strings representing the labels of the dataset.
-            number_of_classes: An integer representing the number of classes in the dataset.
-        Returns:
-            A torch.Tensor object representing the encoded labels.
-        """
-        encoded_labels = torch.zeros(number_of_classes)
-        for label in labels:
-            encoded_labels[int(label)] = 1
-        return encoded_labels
-
-
-    def train(self, epochs: int, train_dataloader: DataLoader, val_dataloader: DataLoader, verbose : bool = True) -> torchvision.models:
-        #SOURCE: https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
-        """
-        This method is used to train the neural network.
-        Args:
-            epochs: An integer representing the number of epochs to train the neural network.
-            train_dataloader: A torch.utils.data.dataloader.DataLoader object representing the training set.
-            val_dataloader: A torch.utils.data.dataloader.DataLoader object representing the validation set.
-            verbose: A boolean representing whether to print the training progress or not.
-        Returns:
-            A torchvision.models object representing the trained neural network.
-        """
-        since = time_time()
-        best_model_wts = self._model.state_dict()
-        best_acc = 0.0
-
-        for epoch in range(epochs):
-            
-            if verbose:
-                print("Epoch {}/{}".format(epoch, epochs - 1))
-                print("-" * 10)
-            for phase in ["train", "val"]:
-                if phase == "train":
-                    self._model.train()
-                else:
-                    self._model.eval()
-
-            running_loss = 0.0
-            running_corrects = 0
-
-            pass
-        pass
-
-
-if __name__ == "__main__":
-    model = None
-    criterion = None
-    optimizer = None
-    scheduler = None
-    train_dogs_net = TrainDogsNet(model, criterion, optimizer, scheduler)
-    labels = train_dataloader.get_labels()
-    number_of_classes = len(set(labels))
-    train_dogs_net._enconde_labels(labels, number_of_classes)
-    pass
+    training_class = TrainDogsNet(
+        model=neural_net_parameters["model"],
+        optimizer=neural_net_parameters["optimizer"],
+        scheduler=neural_net_parameters["scheduler"],
+        criterion=neural_net_parameters["criterion"],
+    )
